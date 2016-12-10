0b86032f1413538faa58fc203594faccab25d917
Orestis Markou <orestis@orestis.gr>
2016-12-10 13:20:27 +0100

    pipe the plaintext through a do-nothing pipeline

--- frameworks/Elixir/phoenix/web/router.ex
+++ frameworks/Elixir/phoenix/web/router.ex
@@ -1,13 +1,20 @@
 defmodule Hello.Router do
   use Hello.Web, :router
 
+  pipeline :bare do
+  end
+
+  scope "/", Hello do
+    pipe_through :bare
+    get "/plaintext", PageController, :plaintext
+  end
+
   scope "/", Hello do
     get "/json", PageController, :_json
     get "/db", PageController, :db
     get "/queries", PageController, :queries
     get "/fortune", PageController, :fortunes
     get "/update", PageController, :updates
-    get "/plaintext", PageController, :plaintext
     get "/", PageController, :index
   end
 end
